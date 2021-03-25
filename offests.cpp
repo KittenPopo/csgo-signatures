2bfa03fb5098e9b4faa1c5aeb8f3b0baa94e3f74
KittenPopo <Pokeberry123@gmail.com>
2021-03-25 05:39:46 -0700

    Fixed a typo

--- offests.cpp
+++ offests.cpp
@@ -5,6 +5,6 @@
 	(OFFSET) ModelPtr = this + ["8B B6 ? ? ? ? 85 F6 74 48" + 2]; // Model pointer from player
 	(OFFSET) MostRecentModelBoneCounter = this + ["8B 86 ? ? ? ? 3B 05 ? ? ? ? 74" + 2]; // m_iMostRecentModelBoneCounter from player
 	(OFFSET) SpawnTime = this + ["89 87 ? ? ? ? 89 B7 ? ? ? ? 89" + 2]; // Spawn time from player
-	(OFFSET) achedBoneData = *(this + ["FF B7 ? ? ? ? 52" + 2]) + 4; // m_CachedBoneData from player
+	(OFFSET) CachedBoneData = *(this + ["FF B7 ? ? ? ? 52" + 2]) + 4; // m_CachedBoneData from player
 	(PTR) ModelBoneCounter = *(["80 3D ? ? ? ? ? 74 16 A1 ? ? ? ? 48 C7 81" + 10]); // g_iModelBoneCounter
 }
